0854e61c94d94cce8e83345fde9a08656c5a5d63
juaniagasse44 <juani_agasse@hotmail.com>
2025-08-19 11:35:28 -0300

    Implemente API REST completa para Productos

--- src/main/java/com/servicioGestion/supermercado/apiREST/Repositories/DetallePedidoRepository.java
+++ src/main/java/com/servicioGestion/supermercado/apiREST/Repositories/DetallePedidoRepository.java
@@ -1,9 +1,11 @@
 package com.servicioGestion.supermercado.apiREST.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.servicioGestion.supermercado.apiREST.Models.DetallePedido;
 
+@Repository
 public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Integer> {
 
 }
